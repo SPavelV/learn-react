193f8ecddf770a197678b9d4c3b5a7eca7d82b96
SPavelV <pashius2010@gmail.com>
2018-11-26 11:43:40 +0400

    refs in functional components

--- color-organizer/src/components/AddColorForm.js
+++ color-organizer/src/components/AddColorForm.js
@@ -1,40 +1,66 @@
 import React,{Component} from 'react'
 import PropTypes from 'prop-types'
 
-class AddColorForm extends Component {
-  constructor(props) {
-    super(props);
-    this.submit = this.submit.bind(this);
-  }
 
-  submit(e) {
-    const {_title, _color} = this.refs;
+//Example functional components:
+const AddColorForm = ({onNewColor=f=>f}) => {
+  let _title, _color;
+  const submit = e => {
     e.preventDefault();
-    this.props.onNewColor(_title.value, _color.value);
+    onNewColor(_title.value, _color.value);
     _title.value = '';
     _color.value = '#000000';
     _title.focus();
-  }
-
-  render() {
-    return (
-      <form onSubmit={e=>e.preventDefault()}>
-        <input type="text"
-               placeholder="color title..." required/>
-        <input type="color" required/>
-        <button>ADD</button>
-      </form>
-    )
-  }
-}
-
-AddColorForm.propTypes ={
-  onNewColor: PropTypes.func
+  };
+  return (
+    <form onSubmit={submit}>
+      <input ref={input => _title = input}
+             type="text"
+             placeholder="color title..." required/>
+      <input ref={input => _color = input}
+             type="color" required/>
+      <button>ADD</button>
+    </form>
+   )
 };
-
-AddColorForm.defaultProps ={
-  onNewColor: f=>f
-};
-
 
 export default AddColorForm;
+
+// Example React.Component
+// class AddColorForm extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.submit = this.submit.bind(this);
+//   }
+//
+//   submit(e) {
+//     const {_title, _color} = this.refs;
+//     e.preventDefault();
+//     this.props.onNewColor(_title.value, _color.value);
+//     _title.value = '';
+//     _color.value = '#000000';
+//     _title.focus();
+//   }
+//
+//   render() {
+//     return (
+//       <form onSubmit={e=>e.preventDefault()}>
+//         <input type="text"
+//                placeholder="color title..." required/>
+//         <input type="color" required/>
+//         <button>ADD</button>
+//       </form>
+//     )
+//   }
+// }
+//
+// AddColorForm.propTypes ={
+//   onNewColor: PropTypes.func
+// };
+//
+// AddColorForm.defaultProps ={
+//   onNewColor: f=>f
+// };
+//
+//
+// export default AddColorForm;
