94cbb80976f6ce38c8a0f16c3a0ba54378f02f3a
PavelSolovev <pashius2010@gmail.com>
2018-11-27 08:04:25 +0400

    add news-app

--- color-organizer/src/components/AddColorForm.js
+++ color-organizer/src/components/AddColorForm.js
@@ -1,40 +1,47 @@
 import React,{Component} from 'react'
 import PropTypes from 'prop-types'
 
-class AddColorForm extends Component {
-  constructor(props) {
-    super(props);
-    this.submit = this.submit.bind(this);
+//Functional components example:
+const AddColorForm = ({onNewColor=f=>f}) => {
+  let _title, _color;
+  const submit = e => {
+
   }
-
-  submit(e) {
-    const {_title, _color} = this.refs;
-    e.preventDefault();
-    this.props.onNewColor(_title.value, _color.value);
-    _title.value = '';
-    _color.value = '#000000';
-    _title.focus();
-  }
-
-  render() {
-    return (
-      <form onSubmit={e=>e.preventDefault()}>
-        <input type="text"
-               placeholder="color title..." required/>
-        <input type="color" required/>
-        <button>ADD</button>
-      </form>
-    )
-  }
-}
-
-AddColorForm.propTypes ={
-  onNewColor: PropTypes.func
-};
-
-AddColorForm.defaultProps ={
-  onNewColor: f=>f
 };
 
 
-export default AddColorForm;
+//React.Component example:
+// export default class AddColorForm extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.submit = this.submit.bind(this);
+//   }
+//
+//   submit(e) {
+//     const {_title, _color} = this.refs;
+//     e.preventDefault();
+//     this.props.onNewColor(_title.value, _color.value);
+//     _title.value = '';
+//     _color.value = '#000000';
+//     _title.focus();
+//   }
+//
+//   render() {
+//     return (
+//       <form onSubmit={e=>e.preventDefault()}>
+//         <input type="text"
+//                placeholder="color title..." required/>
+//         <input type="color" required/>
+//         <button>ADD</button>
+//       </form>
+//     )
+//   }
+// }
+//
+// AddColorForm.propTypes ={
+//   onNewColor: PropTypes.func
+// };
+//
+// AddColorForm.defaultProps ={
+//   onNewColor: f=>f
+// };
